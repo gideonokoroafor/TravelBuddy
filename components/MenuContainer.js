3dc62aa10f84024275c099af31d57216e78c156c
Gideon Okoroafor <okoro062@umn.edu>
2023-07-03 01:13:49 -0500

    menu container

---
+++ components/MenuContainer.js
@@ -0,0 +1,19 @@
+import { Image, View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const MenuContainer = ({key, title, imageSrc, type, setType}) => {
+  const handlePress = () => {
+    setType(title.toLowerCase())
+  }  
+  return (
+    <TouchableOpacity className="items-center justify-center space-y-2" onPress={handlePress}>
+      <View className={`w-24 h-24 shadow-sm p-2 rounded-full items-center justify-center 
+      ${type === title.toLowerCase() ? "bg-gray-200":"" }`}>
+        <Image source={imageSrc} className="w-full h-full object-contain"/>
+      </View>
+      <Text className="text-[#00BCC9] text-xl font-semibold">{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default MenuContainer
