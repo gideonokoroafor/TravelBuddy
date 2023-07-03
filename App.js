879478465f9af98a784b804828b536488efe69dd
Gideon Okoroafor <okoro062@umn.edu>
2023-07-03 01:10:47 -0500

    implemented details page

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text, View } from "react-native";
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import WelcomeScreen from './screens/Welcome';
 import Discover from './screens/Discover';
+import PlacesDetailsScreen from './screens/PlacesDetailsScreen';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -12,8 +11,9 @@ export default function App() {
     <TailwindProvider>
       <NavigationContainer>
         <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Discover" component={Discover}/>
+        <Stack.Screen name="PlacesDetails" component={PlacesDetailsScreen}/>
       </Stack.Navigator>
       </NavigationContainer>
     </TailwindProvider>
